9621cb0289c6179bdf1b24cc4f158f158e0e2826
Joan Llobet <83411395+srLlobet@users.noreply.github.com>
2024-05-16 21:11:33 +0200

    Update BlockScript.cpp
    fix

--- BlockScript.cpp
+++ BlockScript.cpp
@@ -3,19 +3,9 @@
 void BlockScript::tickScript(float deltaTime) {
 
 	// t + 1segon
-	t += deltaTime / 1000;
-
-	//exemple de timer per power ups
-	if (timer <= 0) {
-		//fas algo
-		timer += 10000;
-	}
-	else {
-		timer -= deltaTime;
-	}
+	t += deltaTime / 100;
 
 
-	//glm::vec2 currDir(-1., 0.);
 
 	ComponentHandle<BoxCollider> collider = entity->get<BoxCollider>();
 	ComponentHandle<Transform> transform = entity->get<Transform>();
@@ -37,11 +27,7 @@ void BlockScript::tickScript(float deltaTime) {
 		collider->collidedWith = false;
 	}
 
-	if(t<20)
-	transform->position += currDir * (t/20);
-	else {
 
-		transform->position += currDir;
-	}
+	transform->position += currDir * (t / 50);
 
 }
